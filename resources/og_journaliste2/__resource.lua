445f90e13372c6e1c03a0bf221692a261afcd2d4
Unknown <minne.emeric@gmail.com>
2017-05-21 15:42:26 +0200

    Journaliste v2.0 base ambulancier
    v2.0

---
+++ resources/og_journaliste2/__resource.lua
@@ -0,0 +1,10 @@
+-- Manifest
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
+-- Requiring essentialmode
+dependency 'essentialmode'
+
+-- Emergency
+client_script 'client/cjl_journaliste.lua'
+
+server_script 'server/sjl_journaliste.lua'
